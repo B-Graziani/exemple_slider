d9ab3b2afe6320425669a18776cf6e6817fa914f
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-02-26 15:52:42 +0100

    done

---
+++ app/javascript/components/slider-auto.js
@@ -0,0 +1,11 @@
+const sliderAuto = () => {
+   var counter = 1;
+   setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4) {
+      counter = 1;
+    }
+   }, 3000);
+}
+export {sliderAuto};
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { sliderAuto } from'../components/slider-auto';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -31,4 +32,5 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  sliderAuto();
 });
